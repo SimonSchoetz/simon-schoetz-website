766d2549ac4a7ae714530a77d4398d5096b4a5e3
Simon <simonsch.tz@gmail.com>
2020-08-08 02:25:07 +0200

    Infrastructure for Discography

--- src/Producer.js
+++ src/Producer.js
@@ -10,6 +10,26 @@ export default function Producer() {
         setCurrentLocation(locations.pathname)
     }, [setCurrentLocation])
 
+    const discography = [
+        {id: 0, name: "OMDHA Duck Trumpet RMX", release: "Let's Cut Some Wood EP", free: true, link: "https://bustedfingerz.bandcamp.com/album/lets-cut-some-wood-ep"},
+        {id: 1, name: "Bittersweet", release: "Let's Cut Some Wood EP", free: true, link: "https://bustedfingerz.bandcamp.com/album/lets-cut-some-wood-ep"},
+        {id: 2, name: "OMDHA Duck Trumpet RMX", release: "Let's Cut Some Wood EP", free: true, link: "https://bustedfingerz.bandcamp.com/album/lets-cut-some-wood-ep"},
+
+    ]
+
+    const renderDiscography = () => (
+        discography.map(el => {
+              return  <li key={el.id}>
+                    <ul className="release">
+                        <li>{el.name}</li>
+                        <li>{el.release}</li>
+                        <li><a target="_blank" rel="noopener noreferrer" href={el.link}>{el.free ? "Free DL" : "Buy Here"}</a></li>
+                    </ul>
+                </li>
+
+        }))
+ 
+
     return (
         <DocumentTitle title ="Busted Fingerz">
             <div className="producer">
@@ -21,6 +41,9 @@ export default function Producer() {
                 </section>
                 <section className="discography">
                 <h2>DISCOGRAPHY</h2>
+                <ul>
+                    {renderDiscography()}
+                </ul>
                 
 
                 </section>

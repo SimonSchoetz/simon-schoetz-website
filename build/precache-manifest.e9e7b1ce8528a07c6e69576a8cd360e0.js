bc1df96f78b6904ac84675d374d630cb14225ca0
Simon <simonsch.tz@gmail.com>
2020-08-28 12:51:24 +0200

    Beta Build Version #1

---
+++ build/precache-manifest.e9e7b1ce8528a07c6e69576a8cd360e0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaab8b29c38463c250004ffa25ed3ee2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3715c44878e02aa40cff",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "e96faacf8beea978abb9",
+    "url": "/static/css/main.231184af.chunk.css"
+  },
+  {
+    "revision": "3715c44878e02aa40cff",
+    "url": "/static/js/2.2a3a5d49.chunk.js"
+  },
+  {
+    "revision": "6e900953f82e75bb7ccced89e6806918",
+    "url": "/static/js/2.2a3a5d49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e96faacf8beea978abb9",
+    "url": "/static/js/main.34bcb62b.chunk.js"
+  },
+  {
+    "revision": "afa57f41635acb53feab",
+    "url": "/static/js/runtime-main.fa2c0e34.js"
+  },
+  {
+    "revision": "9e6b41f587301d895e5a4c87688626d5",
+    "url": "/static/media/BellGothicStd-Black.9e6b41f5.otf"
+  },
+  {
+    "revision": "1f8feea249734f162ca07174ca5dd414",
+    "url": "/static/media/BellGothicStd-Bold.1f8feea2.otf"
+  },
+  {
+    "revision": "66c20974c560c487ff6335f108f67447",
+    "url": "/static/media/BellGothicStd-Light.66c20974.otf"
+  },
+  {
+    "revision": "35ccd85b90b8e1661b426aa3aa9e597c",
+    "url": "/static/media/at-dorian-gray-studios.35ccd85b.png"
+  },
+  {
+    "revision": "03bb870883618a4281d5afada30072ae",
+    "url": "/static/media/busted_fingerz_live.03bb8708.png"
+  },
+  {
+    "revision": "8ac5a4c942b50ae57653a7e0236141f4",
+    "url": "/static/media/etikett-home.8ac5a4c9.png"
+  }
+]);

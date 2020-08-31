c7b5c4d04c7fbc702b0d90aff4006259f3e1d441
Simon <simonsch.tz@gmail.com>
2020-08-31 13:04:57 +0200

    Finish Etikett Radio for now

--- src/WebDev.js
+++ src/WebDev.js
@@ -20,7 +20,7 @@ export default function WebDev() {
         {id: 6, name: "Node.js", img: "node-logo.png", level: 3},
         {id: 1, name: "Express.js", img: "express-logo.png", level: 3},
         {id: 5, name: "MongoDB", img: "mongodb-logo.png", level: 3},
-        {id: 7, name: "npm", img: "npm-logo.png", level: 2},
+        {id: 7, name: "npm", img: "npm-logo.png", level: 3},
         {id: 2, name: "GitHub", img: "github-logo.png", level: 4},
         {id: 10, name: "Three.js", img: "threejs-logo.png", level: 1},
         {id: 11, name: "Vue.js", img: "vuejs-logo.png", level: 1},
@@ -47,13 +47,13 @@ export default function WebDev() {
     const renderSkillSet = () => (
         skillSet.map(el => {
             return <Fragment key={el.id}>
-                <li>
-                <div className="img-container">
-                    <img alt={`Logo of ${el.name}`} title={el.name} className="tech-icon" src={require("./img/"+el.img)} />
-                </div>
-                <div className="level-container">
-                {fullStars(el.level)}{emptyStars(el.level)}
-                </div>
+                <li title={el.name}>
+                    <div className="img-container">
+                        <img alt={`Logo of ${el.name}`} className="tech-icon" src={require("./img/"+el.img)} />
+                    </div>
+                    <div className="level-container">
+                    {fullStars(el.level)}{emptyStars(el.level)}
+                    </div>
                 </li>
             </Fragment>
 
@@ -63,8 +63,8 @@ export default function WebDev() {
         <DocumentTitle title ="Web Developer">
             <div className="web-dev">
                 <section className="web-def-intro">
-                <h2>Profile</h2>
-                    <h3>Professional Experience</h3>
+                <h2>PROFILE</h2>
+                    <h3>PROFESSIONAL EXPERIENCE</h3>
                     <div className="content-container" >
                         <div className="time-line">
                             <div className="code-window">
@@ -94,8 +94,33 @@ export default function WebDev() {
                     </div>
 
                 </section>
-                <section className="web-def-ref">
-                    <div className="etikett-radio">
+                <section className="web-dev-ref">
+                    <h2>REFERENCES</h2>
+                    <h3>etikett~radio</h3>
+                    <div className="ref-container etikett-radio">
+                        <div className="ref-description">
+                            <h3>DESCRIPTION</h3>
+                            <p>
+                                etikett~radio is a radio station at Catalyst Institute (former dBs Music School Berlin) where students have the chance to gain their first experience as radio hosts. It has a database where admins can create accounts for students. Students can then work on their host profile and list their shows in the archive section, post to the blog section and so on. Moreover, the website has its own chat room where every user of the website can join and interact with each other.
+                            </p>
+                            <h3>LINKS</h3>
+                            <ul>
+                                <li>
+                                    <a target="_blank" rel="noopener noreferrer" href={`https://etikett-radio.herokuapp.com/`}>www.etikett-radio.com</a>
+                                </li>
+                                <li>
+                                    <a target="_blank" rel="noopener noreferrer" href={`https://github.com/SimonSchoetz/etikett-radio`}>GitHub Repo</a>
+                                </li>
+                            </ul>
+                            <h3>MAIN TECHNOLOGIES</h3>
+                            <ul>
+                               <li>Frontend: React.js, SASS</li>
+                               <li>Backend: MongoDB, Mongoose, Express.js, JWT</li>
+                               <li>Other: Sockit.io, Three.js</li>
+                            </ul>
+
+                        </div>
+
                     </div>
                 </section>
                 

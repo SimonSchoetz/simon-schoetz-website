432a89805c31dfbaf0762f21f54df8deac2ce1a2
Simon <simonsch.tz@gmail.com>
2020-11-11 19:37:16 +0100

    Reset works

--- src/ProducerCards.js
+++ src/ProducerCards.js
@@ -1,7 +1,7 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, Fragment } from 'react';
 import { Context } from './Context';
 
-export default function ProducerCards({el}) {    
+export default function ProducerCards({el, i}) {    
     const {
     setMouseOverElement
 } = useContext(Context)
@@ -11,14 +11,17 @@ export default function ProducerCards({el}) {
         const offsetX = input.clientX - rect.left;
         const offsetY  = input.clientY - rect.top;
         setImgOffset({top: offsetY, left: offsetY})
+        console.log(input.type)
+        if (input.type === "mouseleave") {
+            setImgOffset({top: 0, left: 0})
+        }
     }
     const handleMouseOverElement = (input) => {
         if (input.type === "mouseenter") {setMouseOverElement(true)}
         else setMouseOverElement(false)
     }
-    console.log(el)
     return (
-        <div>
+        <Fragment key={i}>
             <li title={`${el.name}`} key={el.id} className="ref-card" onMouseMove={handleCoordinates} onMouseLeave={handleCoordinates}>
                 <div className="content-container">
                     <a target="_blank" rel="noopener noreferrer" href={el.link} onMouseEnter={handleMouseOverElement} onMouseLeave={handleMouseOverElement}>
@@ -30,6 +33,6 @@ export default function ProducerCards({el}) {
                     </a>
                 </div>
             </li>
-        </div>
+        </Fragment>
     )
 }

aadc64a2cd90c96f0095df1249716d78bcbed9de
Simon <simonsch.tz@gmail.com>
2020-11-11 20:09:51 +0100

    it fuckin works oh my gash

--- src/ProducerCards.js
+++ src/ProducerCards.js
@@ -2,24 +2,32 @@ import React, { useState, useContext, Fragment } from 'react';
 import { Context } from './Context';
 
 export default function ProducerCards({el, i}) {    
-    const {
-    setMouseOverElement
-} = useContext(Context)
+    const {setMouseOverElement} = useContext(Context)
+
     const [imgOffset, setImgOffset] = useState({top: 0, left: 0})
+
     const handleCoordinates = (input) => {
+        setImgOffset({top: -12.5+"%", left: -12.5+"%"})
         const rect = input.target.getBoundingClientRect();
-        const offsetX = input.clientX - rect.left;
-        const offsetY  = input.clientY - rect.top;
-        setImgOffset({top: offsetY, left: offsetY})
-        console.log(input.type)
+        const positionY  = input.clientY - rect.top;
+        const positionX = input.clientX - rect.left;
+        const offsetY = (25/ 100)  * (100 / rect.height * positionY) //in Prozent
+        const offsetX = (25/ 100)  * (100 / rect.height * positionX) //in Prozent
+        if (input.target.tagName === "IMG") {
+             setImgOffset({top: (-25+offsetY)+"%", left: (-25+offsetX)+"%"})
+        }
+        
+       
         if (input.type === "mouseleave") {
             setImgOffset({top: 0, left: 0})
         }
     }
+
     const handleMouseOverElement = (input) => {
         if (input.type === "mouseenter") {setMouseOverElement(true)}
         else setMouseOverElement(false)
     }
+
     return (
         <Fragment key={i}>
             <li title={`${el.name}`} key={el.id} className="ref-card" onMouseMove={handleCoordinates} onMouseLeave={handleCoordinates}>

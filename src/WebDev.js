27ff5f8a443462815810d2abde0edaa1bac281cc
Simon <simonsch.tz@gmail.com>
2020-12-04 11:35:53 +0100

    Build O and make it consist only of percentage

--- src/WebDev.js
+++ src/WebDev.js
@@ -76,6 +76,7 @@ export default function WebDev() {
                     <div className="t-roof-l"/>
                     <div className="t-roof-r"/>
                     <div className="t-neck"/>
+                    <div className="o-body"/>
                     <p className="word-to">TO</p>
                     <p className="word-top">TOP</p>
 
